9fa6aa0a113331684ac5f86bd30e807da4c189a7
yatharthasharma <yatharthasharma09@gmail.com>
2023-02-01 08:41:20 +0000

    add the solution

--- src/com/thoughtworks/main/Account.java
+++ src/com/thoughtworks/main/Account.java
@@ -23,9 +23,34 @@ public class Account {
     }
 
     public Account(int accountId, int balance, Currency currency, int overdraftLimit) {
+        if (balance < 0) {
+            throw new InsufficientBalanceException("Unable to open an account with a negative balance!");
+        }
         this.accountId = accountId;
         this.balance = balance;
         this.currency = currency;
         this.overdraftLimit = overdraftLimit;
+    }
+
+    public void currencyChecks(Currency transactionCurrency) {
+        if (currency != transactionCurrency) {
+            throw new InvalidCurrencyException("This account does not support international transactions!");
+        }
+    }
+
+    public int debit(int debitAmount, Currency debitCurrency) {
+        currencyChecks(debitCurrency);
+        if (balance < debitAmount) {
+            throw new InsufficientBalanceException("The account has insufficient funds.");
+        }
+        balance -= debitAmount;
+        return balance;
+
+    }
+
+    public int credit(int creditAmount, Currency depositCurrency) {
+        currencyChecks(depositCurrency);
+        balance += creditAmount;
+        return balance;
     }
 }
--- src/com/thoughtworks/main/PaymentService.java
+++ src/com/thoughtworks/main/PaymentService.java
@@ -2,28 +2,18 @@ package com.thoughtworks.main;
 
 public class PaymentService {
     AccountRepository accountRepository;
+
     public PaymentService(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
     }
 
-    public int debit(int customerId, int amount, Currency depositCurrency) throws InvalidCurrencyException, InsufficientBalanceException {
+    public int debit(int customerId, int amount, Currency debitCurrency) throws InsufficientBalanceException, InvalidCurrencyException {
         Account account = accountRepository.getAccountByCustomer(customerId);
-        if (account.getCurrency().equals(depositCurrency)) {
-            if (account.getBalance() < amount) {
-                throw new InsufficientBalanceException("The account has insufficient funds.");
-            }
-            account.setBalance(account.getBalance() - amount);
-            return account.getBalance();
-        }
-        throw new InvalidCurrencyException("This account does not support international transactions!");
+        return account.debit(amount, debitCurrency);
     }
 
     public int credit(int customerId, int amount, Currency depositCurrency) throws InvalidCurrencyException {
         Account account = accountRepository.getAccountByCustomer(customerId);
-        if (account.getCurrency().equals(depositCurrency)) {
-            account.setBalance(account.getBalance() + amount);
-            return account.getBalance();
-        }
-        throw new InvalidCurrencyException("Invalid currency deposited");
+        return account.credit(amount, depositCurrency);
     }
 }
